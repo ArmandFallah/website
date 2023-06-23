7650bbf63f2d71b440144f5b610a96be3d662372
Armand Fallah <armand.fallah@gmail.com>
2023-06-23 12:17:47 -0400

    simplify and refactor

--- src/consts.ts
+++ src/consts.ts
@@ -1,41 +1,35 @@
 import type { Site, Metadata, Socials } from '@types';
 
 export const SITE: Site = {
-  NAME: 'Armand World',
-  EMAIL: 'armand@fallah.us',
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  name: 'Armand World',
+  email: 'armand@fallah.us',
+  num_posts_on_homepage: 3,
+  num_projects_on_homepage: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: 'Home',
-  DESCRIPTION: 'Armand World ',
+  title: 'Home',
+  description: 'Armand World ',
 };
 
 export const BLOG: Metadata = {
-  TITLE: 'Blog',
-  DESCRIPTION: 'A collection of articles on topics I am passionate about.',
-};
-
-export const WORK: Metadata = {
-  TITLE: 'Work',
-  DESCRIPTION: 'Where I have worked and what I have done.',
+  title: 'Blog',
+  description: 'A collection of articles on topics I am passionate about.',
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: 'Projects',
-  DESCRIPTION:
+  title: 'Projects',
+  description:
     'A collection of my projects, with links to repositories and demos.',
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: 'github',
-    HREF: 'https://github.com/armandfallah',
+    name: 'github',
+    href: 'https://github.com/armandfallah',
   },
   {
-    NAME: 'linkedin',
-    HREF: 'https://www.linkedin.com/in/armand-fallah-8a01a6301/',
+    name: 'linkedin',
+    href: 'https://www.linkedin.com/in/armand-fallah-8a01a6301/',
   },
 ];
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,16 @@
 export type Site = {
-  NAME: string;
-  EMAIL: string;
-  NUM_POSTS_ON_HOMEPAGE: number;
-  NUM_WORKS_ON_HOMEPAGE: number;
-  NUM_PROJECTS_ON_HOMEPAGE: number;
+  name: string;
+  email: string;
+  num_posts_on_homepage: number;
+  num_projects_on_homepage: number;
 };
 
 export type Metadata = {
-  TITLE: string;
-  DESCRIPTION: string;
+  title: string;
+  description: string;
 };
 
 export type Socials = {
-  NAME: string;
-  HREF: string;
+  name: string;
+  href: string;
 }[];
